52b17c7d2049b02413a1280c56e602c8df8e8e76
sergey <sergey@wikia-inc.com>
2014-05-01 23:36:06 +0200

    fixes Meaningful share buttons #3

--- src/App/Resources/app/views/homepage-view.js
+++ src/App/Resources/app/views/homepage-view.js
@@ -2,7 +2,10 @@ define([ 'app/globals', 'chaplin', 'app/lib/utils', 'jquery', './dynamic-view', 
 
 	return DynamicView.extend({
 		events: {
-			'click .splash-get-started-text a': 'eventGetStartedClick'
+			'click .splash-get-started-text a': 'eventGetStartedClick',
+			'click .social-icons .twitter': 'shareTwitter',
+			'click .social-icons .facebook': 'shareFacebook',
+			'click .social-icons .plus': 'sharePlus'
 		},
 		eventGetStartedClick: function(e) {
 
@@ -20,6 +23,29 @@ define([ 'app/globals', 'chaplin', 'app/lib/utils', 'jquery', './dynamic-view', 
 				this.subview('random-block', new HomepageRandomView());
 			});
 
+		},
+		shareTwitter: function(e) {
+			console.log(this.arguments);
+
+			e.currentTarget.href = [
+				'https://twitter.com/share?text=',
+				encodeURIComponent('I\'ve downloaded all my instagram photos thanks to this site!'), ' ',
+				encodeURIComponent(utils.reverse('homepage')),
+			].join('');
+
+		},
+		shareFacebook: function(e) {
+
+			e.currentTarget.href = [
+				'https://www.facebook.com/sharer/sharer.php?u=',
+				encodeURIComponent(utils.reverse('homepage')),
+			].join('');
+		},
+		sharePlus: function(e) {
+			e.currentTarget.href = [
+				'https://plus.google.com/share?url=',
+				encodeURIComponent(utils.reverse('homepage')),
+			].join('');
 		}
 	});
 });
